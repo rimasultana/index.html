400608ea28908aed88402a198dbf668627142d19
rimasultana <rimasultana20225@gmail.com>
2024-04-07 06:38:53 +0600

    window

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //  select Dom Element
+let myWindow;
 
 const width = document.getElementById("width");
 const height = document.getElementById("height");
 
 
 width.innerHTML = "Window inner width is : " + window.innerWidth;
-height.innerHTML = "Window inner height is : " + window.innerHeight;
+
+height.innerHTML = "Window inner is : " + window.innerHeight;
+
+function openWindow(){
+    window.open("https://goggle.com");
+}
+
+function closeWindow() {
+    myWindow.close();
+}
