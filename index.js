bd7b4077fd525900911072d689003766f87053ff
rimasultana <rimasultana20225@gmail.com>
2024-04-07 06:28:59 +0600

    BOm window

---
+++ index.js
@@ -0,0 +1,8 @@
+//  select Dom Element
+
+const width = document.getElementById("width");
+const height = document.getElementById("height");
+
+
+width.innerHTML = "Window inner width is : " + window.innerWidth;
+height.innerHTML = "Window inner height is : " + window.innerHeight;
